b63f4db68897dc0a2a8cf47b0cde887c10ee9f2d
kazupusan <k.n.777326@gmail.com>
2018-02-21 09:38:39 +0000

    kadai-bootstrap

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,16 +3,27 @@
 @section('content')
   <h1>id: {{ $task->id }} の編集ページ</h1>
 
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス') !!}
-        {!! Form::text('status') !!}
+    <div class="row">
+        <div class="col-xs-12">
+         <div class="col-sm-offset-2 col-sm-8 col-sm-offset-2">
+          <div class="col-lg-offset-3 col-lg-6 col-lg-offset-3">  
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
         
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
+             </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
